b6473642aaaac87c1a921b201e906126585c25fd
wannpwt <51039540+wannpwt@users.noreply.github.com>
2019-05-30 11:46:56 +0700

    [ADD]create project

---
+++ WIP-XI/src/IV/Operators/_3_Logical.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package IV.Operators;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _3_Logical {
+    public static void main(String[] args) {
+        int x = 5;
+        
+        System.out.println("|| : " + (x < 4 || x > 4));
+        
+        System.out.println("&& : " + (x > 4 && x < 4));
+        
+        System.out.println("Normal : " + (x >= 5));
+        
+        System.out.println("! : " + !(x >= 5));
+    }
+}
